0da00eaff164112464b44c2525e89c7f14db559b
Vinicius Goneli <viniciusgoneli_2012@hotmail.com>
2020-09-27 15:57:55 -0300

    Category entity

--- src/main/java/com/vallosstudio/workshop/config/TestConfig.java
+++ src/main/java/com/vallosstudio/workshop/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.vallosstudio.workshop.entities.Category;
 import com.vallosstudio.workshop.entities.Order;
 import com.vallosstudio.workshop.entities.User;
 import com.vallosstudio.workshop.enums.OrderStatus;
+import com.vallosstudio.workshop.repositories.CategoryRepository;
 import com.vallosstudio.workshop.repositories.OrderRepository;
 import com.vallosstudio.workshop.repositories.UserRepository;
 
@@ -23,17 +25,24 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		User u1 = new User(null, "João da Silva", "joao@gmail.com", "1588442255", "2122545");
 		User u2 = new User(null, "Mariana Luz", "mariana@gmail.com", "1587889963", "2556512");
 		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1, OrderStatus.WAITING_PAYMENT);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2, OrderStatus.PAID);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.SHIPPED); 
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
 }
---
+++ src/main/java/com/vallosstudio/workshop/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.vallosstudio.workshop.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vallosstudio.workshop.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
