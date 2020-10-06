1b553cad53ed87dd512205f8805253e880fd7ede
Vinicius Goneli <viniciusgoneli_2012@hotmail.com>
2020-10-05 21:31:05 -0300

    Subtotal & Total methods

--- src/main/java/com/vallosstudio/workshop/entities/Order.java
+++ src/main/java/com/vallosstudio/workshop/entities/Order.java
@@ -102,6 +102,14 @@ public class Order implements Serializable {
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
+	
+	public Double total() {
+		double total = 0.0;
+		for(OrderItem item : items) {
+			total += item.subTotal();
+		}
+		return total;
+	}
 
 	@Override
 	public int hashCode() {
